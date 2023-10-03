49c930062beac19df0343913405cfc31b6512e61
Tim Fournet <tim@rewst.io>
2023-10-03 18:01:03 -0500

    gracefully handle config file
    Signed-off-by: Tim Fournet <tim@rewst.io>

--- rewst_remote_agent.py
+++ rewst_remote_agent.py
@@ -3,10 +3,27 @@ import json
 from azure.iot.device.aio import IoTHubDeviceClient
 
 async def main():
-    # Load configuration from JSON file
-    with open('config.json') as f:
-        config = json.load(f)
+    try:
+        # Attempt to load configuration from JSON file
+        with open('config.json') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        print("Error: Configuration file 'config.json' not found.")
+        return
+    except json.JSONDecodeError:
+        print("Error: Failed to decode JSON from 'config.json'.")
+        return
+    except Exception as e:
+        print(f"Error: An unexpected error occurred while reading 'config.json': {e}")
+        return
     
+    # Ensure all necessary configuration keys are present
+    required_keys = ['azure_iot_hub_host', 'device_id', 'shared_access_key']
+    for key in required_keys:
+        if key not in config:
+            print(f"Error: Missing '{key}' in configuration.")
+            return
+
     # Build the connection string
     conn_str = f"HostName={config['azure_iot_hub_host']};DeviceId={config['device_id']};SharedAccessKey={config['shared_access_key']}"
     
