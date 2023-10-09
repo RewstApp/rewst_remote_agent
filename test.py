1af07cd98e3d1e3287290484b04c10c7fa4f83d6
Tim Fournet <tim@rewst.io>
2023-10-09 16:13:00 -0500

    updates to work cross platform
    Signed-off-by: Tim Fournet <tim@rewst.io>

--- test.py
+++ test.py
@@ -11,27 +11,21 @@ import psutil
 from concurrent.futures import ThreadPoolExecutor
 from azure.iot.device.aio import IoTHubDeviceClient
 
-# Set the status update interval and get the operating system type
 status_update_checkin_time = 600
 os_type = platform.system()
 executor = ThreadPoolExecutor()
 logging.basicConfig(level=logging.INFO)
 
-# Function to send a status update to the IoT Hub
 async def send_status_update():
-    # Collect status data
     status_data = {
         "cpu_usage": psutil.cpu_percent(interval=1),
         "memory_usage": psutil.virtual_memory().percent
     }
-    # Create message object
     message_json = json.dumps(status_data)
-    # Send message
     print("Sending status update to IoT Hub...")
     await device_client.send_message(message_json)
     print("Status update sent!")
 
-# Function to load configuration from a file
 def load_config():
     try:
         with open('config.json') as f:
@@ -39,7 +33,6 @@ def load_config():
     except Exception as e:
         logging.error(f"Error: {e}")
         return None
-    # Check for required keys in the configuration
     required_keys = [
         'azure_iot_hub_host',
         'device_id',
@@ -53,7 +46,6 @@ def load_config():
             return None
     return config
 
-# Function to construct a connection string from the configuration
 def get_connection_string(config):
     conn_str = (
         f"HostName={config['azure_iot_hub_host']};"
@@ -62,24 +54,20 @@ def get_connection_string(config):
     )
     return conn_str
 
-# Handler function for messages received from the IoT Hub
 def message_handler(message):
     print(f"Received message: {message.data}")
     message_data = json.loads(message.data)
     if "commands" in message_data:
         commands = message_data["commands"]
-        post_id = message_data.get("post_id")  # Get post_id, if present
-        interpreter_delimiter = message_data.get("interpreter_delimiter", "\n")  # Get delimiter, if present
+        post_id = message_data.get("post_id")
+        interpreter_delimiter = message_data.get("interpreter_delimiter", "\n")
         print("Running commands")
         executor.submit(run_handle_commands, commands, post_id, None, None, interpreter_delimiter)
 
-# Function to handle the execution of commands
 def run_handle_commands(commands, post_id=None, rewst_engine_host=None, interpreter=None, interpreter_delimiter="\n"):
     asyncio.run(handle_commands(commands, post_id, rewst_engine_host, interpreter, interpreter_delimiter))
 
-# Async function to execute the list of commands
 async def execute_commands(commands, post_url=None, interpreter_override=None, interpreter_delimiter="\n"):
-    # Determine the interpreter based on the operating system
     if os_type == 'windows':
         default_interpreter = 'powershell'
     elif os_type == 'darwin':
@@ -87,75 +75,31 @@ async def execute_commands(commands, post_url=None, interpreter_override=None, i
     else:
         default_interpreter = '/bin/bash'
     interpreter = interpreter_override or default_interpreter
-    # Join commands using the specified delimiter
-    
-    
-    # If PowerShell is the interpreter, update the commands to include the post_url variable
-    if "powershell" in interpreter:
-        preamble = (
-            f"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12\n"
-            f"$post_url = '{post_url}'\n"
-        )
-        # Prepend the preamble to the commands
-        commands = preamble + commands
-
-    # Prepare the command for execution
-    if interpreter_override:
-        # When an interpreter override is specified, prefix the commands with the interpreter
-        command = f'{interpreter} -c "{commands}"'
-    else:
-        # When using the default interpreter, no need to prefix the commands
-        command = commands# Create the command string
+    all_commands = interpreter_delimiter.join(commands)
     command = f'{interpreter} -c "{all_commands}"'
-    # Execute the command
     process = await asyncio.create_subprocess_shell(
         command,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
-    # Gather output
     stdout, stderr = await process.communicate()
-    # Decode output from binary to text
     stdout = stdout.decode('utf-8')
     return stdout.strip()
 
-    # Attempt to parse the command output as JSON
-    try:
-        message_data = json.loads(command_output)
-    except json.JSONDecodeError as e:
-        print(f"Error decoding command output as JSON: {e}")
-        message_data = {"error": f"Error decoding command output as JSON: {e}", "output": command_output}
-    
-    if "powershell" not in interpreter:
-        print("Sending Results to Rewst via httpx.")
-        # Send POST request with command results
-        async with httpx.AsyncClient() as client:
-            response = await client.post(post_url, json=message_data)
-            print(f"POST request status: {response.status_code}")
-            if response.status_code != 200:
-                # Log error information if the request fails
-                print(f"Error response: {response.text}")
-
-
-# Async function to handle the execution of commands and send the output to IoT Hub
 async def handle_commands(commands, post_id=None, rewst_engine_host=None, interpreter_override=None, interpreter_delimiter="\n"):    
     if post_id:
         post_path = post_id.replace(":", "/")
         post_url = f"https://{rewst_engine_host}/webhooks/custom/action/{post_path}"
-    # Execute the commands
     command_output = await execute_commands(commands, rewst_engine_host, interpreter_override, interpreter_delimiter)
     try:
-        # Try to parse the output as JSON
         message_data = json.loads(command_output)
     except json.JSONDecodeError as e:
         print(f"Error decoding command output as JSON: {e}")
         message_data = {"error": f"Error decoding command output as JSON: {e}", "output": command_output}
-    # Send the command output to IoT Hub
     message_json = json.dumps(message_data)
     await device_client.send_message(message_json)
     print("Message sent!")
 
-# Main async function
 async def main(check_mode=False):
     global rewst_engine_host
     config_data = load_config()
@@ -170,7 +114,6 @@ async def main(check_mode=False):
     await device_client.connect()
     print("Connected!")
     if check_mode:
-        # Check mode for testing communication
         print("Check mode: Sending a test message...")
         e = None
         try:
@@ -183,7 +126,6 @@ async def main(check_mode=False):
             await device_client.disconnect()
             exit(0 if not e else 1)
     else:
-        # Set the message handler and start the status update task
         device_client.on_message_received = message_handler
         async def status_update_task():
             while True:
@@ -193,7 +135,6 @@ async def main(check_mode=False):
         stop_event = asyncio.Event()
         await stop_event.wait()
 
-# Entry point of the script
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run the IoT Hub device client.')
     parser.add_argument('--check', action='store_true', help='Run in check mode to test communication')
