a9cac82c418269133199f5252638a7c222e48868
Tim Fournet <tim@rewst.io>
2023-10-09 16:54:46 -0500

    updates

--- compile_for_windows.py
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-dnf -y install wine
-
-wine msiexec /i python-3.11.6-amd64.exe
-
-wine python.exe ~/.wine/drive_c/Python311/Scripts/pip.exe install pyinstaller
-
-wine ~/.wine/drive_c/Python311/Scripts/pyinstaller --onefile rewst_remote_agent.exe
