0f6c0674ba9ceb447bbd53fe50ac6e534a978874
Tim Fournet <tim@rewst.io>
2023-10-26 13:10:22 -0500

    set logging to info
    Signed-off-by: Tim Fournet <tim@rewst.io>

--- rewst_remote_agent.py
+++ rewst_remote_agent.py
@@ -39,7 +39,7 @@ executor = ThreadPoolExecutor()
 
 # Put Timestamps on logging entries
 logging.basicConfig(
-    level=logging.DEBUG,  
+    level=logging.INFO,  
     format='%(asctime)s %(levelname)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
