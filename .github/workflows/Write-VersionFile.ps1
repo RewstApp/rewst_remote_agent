3efb07c6209eee2ae348ad68d1e2276fe3a2e55c
Tim Fournet <tim@rewst.io>
2023-11-01 12:08:11 -0500

    use script to write version file
    Signed-off-by: Tim Fournet <tim@rewst.io>

---
+++ .github/workflows/Write-VersionFile.ps1
@@ -0,0 +1,34 @@
+param(
+    [string]$version
+)
+
+$formatted_version = "${{ needs.bump_version.outputs.new_tag }}" -replace '\.', ','
+
+$versionInfo = @"
+VSVersionInfo(
+    ffi=FixedFileInfo(
+      filevers=($formatted_version,0),
+      prodvers=($formatted_version,0),
+      mask=0x3f,
+      flags=0x0,
+      OS=0x4,
+      fileType=0x1,
+      subtype=0x0,
+      date=(0, 0)
+    ),
+    kids=[
+      StringFileInfo(
+        [
+          StringTable(
+            u'040904b0',
+            [StringStruct(u'FileVersion', u'$version'),
+            StringStruct(u'ProductVersion', u'$version')]
+          )
+        ]
+      ),
+      VarFileInfo([VarStruct(u'Translation', [0x409, 1200])])
+    ]
+  )
+"@
+
+$versionInfo | Out-File -FilePath version.txt -Encoding utf8
