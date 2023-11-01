04c0fedfae0e79d23e7f80b5b32c7339367ce806
Tim Fournet <tim@rewst.io>
2023-11-01 11:45:49 -0500

    build a version.txt for win
    Signed-off-by: Tim Fournet <tim@rewst.io>

--- .github/workflows/build.bat
+++ .github/workflows/build.bat
@@ -1,3 +1,2 @@
 @echo off
-pyinstaller --icon=logo-rewsty.ico --version-file=__version__.py --onefile rewst_remote_agent.py config_module.py service_manager.py
-
+pyinstaller --icon=logo-rewsty.ico --version-file=version.txt --onefile rewst_remote_agent.py config_module.py service_manager.py __version__.py
--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller --version-file=__version__.py --onefile rewst_remote_agent.py config_module.py service_manager.py
+pyinstaller --version-file=__version__.py --onefile rewst_remote_agent.py config_module.py service_manager.py __version__.py
