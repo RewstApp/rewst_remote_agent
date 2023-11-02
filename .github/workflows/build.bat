b814cd334f68b19f70294bc83a987fc0f504b6f4
Tim Fournet <tim@rewst.io>
2023-11-02 17:26:13 -0500

    go back to one-file
    Signed-off-by: Tim Fournet <tim@rewst.io>

--- .github/workflows/build.bat
+++ .github/workflows/build.bat
@@ -1,9 +1,9 @@
 @echo off
 REM Build the first executable
-pyinstaller --name=rewst_remote_agent --icon=logo-rewsty.ico --version-file=version.txt rewst_remote_agent.py
+pyinstaller --name=rewst_remote_agent --icon=logo-rewsty.ico --one-file --version-file=version.txt rewst_remote_agent.py
 
 REM Build the second executable
-pyinstaller --name=rewst_service_manager --icon=logo-rewsty.ico --version-file=version.txt rewst_service_manager.py
+pyinstaller --name=rewst_service_manager --icon=logo-rewsty.ico --one-file --version-file=version.txt rewst_service_manager.py
 
 REM Build the third executable
-pyinstaller --name=rewst_agent_config --icon=logo-rewsty.ico --version-file=version.txt rewst_agent_config.py
+pyinstaller --name=rewst_agent_config --icon=logo-rewsty.ico --one-file --version-file=version.txt rewst_agent_config.py
--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build the first executable
-pyinstaller --name=rewst_remote_agent --icon=logo-rewsty.ico --version-file=version.txt rewst_remote_agent.py
+pyinstaller --name=rewst_remote_agent  --one-file --icon=logo-rewsty.ico --version-file=version.txt rewst_remote_agent.py
 
 # Build the second executable
-pyinstaller --name=rewst_service_manager --icon=logo-rewsty.ico --version-file=version.txt rewst_service_manager.py
+pyinstaller --name=rewst_service_manager --one-file --icon=logo-rewsty.ico --version-file=version.txt rewst_service_manager.py
 
 # Build the third executable
-pyinstaller --name=rewst_agent_config --icon=logo-rewsty.ico --version-file=version.txt rewst_agent_config.py
+pyinstaller --name=rewst_agent_config --one-file --icon=logo-rewsty.ico --version-file=version.txt rewst_agent_config.py
