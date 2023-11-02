501d714162cd5ea8b43c7b60dde0916e6f1bad27
Tim Fournet <tim@rewst.io>
2023-11-02 16:37:07 -0500

    update builds
    Signed-off-by: Tim Fournet <tim@rewst.io>

--- .github/workflows/build.bat
+++ .github/workflows/build.bat
@@ -1,2 +1,9 @@
 @echo off
-pyinstaller --icon=logo-rewsty.ico --version-file=version.txt --onefile rewst_remote_agent.py config_module.py service_manager.py __version__.py
+REM Build the first executable
+pyinstaller --name=rewst_remote_agent --icon=logo-rewsty.ico --version-file=version.txt rewst_remote_agent.py
+
+REM Build the second executable
+pyinstaller --name=rewst_service_manager --icon=logo-rewsty.ico --version-file=version.txt rewst_service_manager.py
+
+REM Build the third executable
+pyinstaller --name=rewst_agent_config --icon=logo-rewsty.ico --version-file=version.txt rewst_agent_config.py
