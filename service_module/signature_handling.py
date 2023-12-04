89c4d82ee5ccb8e3727dd5b57e734ca275a161e9
Tim Fournet <tim@rewst.io>
2023-12-04 11:25:04 -0600

    do checksums for windows binaries t3

--- service_module/signature_handling.py
+++ service_module/signature_handling.py
@@ -1,18 +1,36 @@
-# import win32api
-# import win32con
-# import win32security
+import httpx
 from __version__ import __version__
 
+
 def is_signature_valid(executable_path):
+    repo = "rewstapp/rewst_remote_agent"
+    version_tag = f"v{__version__}"
+    checksum_file_name = f"{executable_path}.sha256"
     # WINTRUST_ACTION_GENERIC_VERIFY_V2 = "{00AAC56B-CD44-11d0-8CC2-00C04FC295EE}"
     try:
-    #     h_file = win32api.CreateFile(executable_path, win32con.GENERIC_READ, win32con.FILE_SHARE_READ, None, win32con.OPEN_EXISTING, win32con.FILE_ATTRIBUTE_NORMAL, None)
-    #     file_info = win32security.WINTRUST_FILE_INFO(h_file, executable_path)
-    #     trust_data = win32security.WINTRUST_DATA(file_info)
-    #     result = win32security.WinVerifyTrust(None, WINTRUST_ACTION_GENERIC_VERIFY_V2, trust_data)
-    #
-    #     return result == 0  # 0 indicates the signature is valid
+
         return True
     except Exception as e:
         print(f"Error verifying signature: {e}")
         return False
+
+
+def get_release_info_by_tag(repo, tag):
+    """
+    Fetch release information from the GitHub repository for a specific tag.
+    """
+    url = f"https://api.github.com/repos/{repo}/releases/tags/{tag}"
+    with httpx.Client() as client:
+        response = client.get(url)
+        response.raise_for_status()
+        return response.json()
+
+def get_checksum_file_url(repo, tag, file_name):
+    """
+    Get the URL of the checksum file for a specific file in a specific release.
+    """
+    release_info = get_release_info_by_tag(repo, tag)
+    for asset in release_info.get("assets", []):
+        if asset["name"] == file_name:
+            return asset["browser_download_url"]
+    return None
