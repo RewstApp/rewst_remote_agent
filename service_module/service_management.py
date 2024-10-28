d5a8d1d7897aa473aa2c93326d2201d8e0e09799
mlataza <1750947+mlataza@users.noreply.github.com>
2024-10-29 05:04:24 +0800

    add docstrings for service_module

--- service_module/service_management.py
+++ service_module/service_management.py
@@ -1,12 +1,14 @@
+""" Module providing helper functions to manage an agent service. """
+
 import logging
 import os
 import platform
-import psutil
 import subprocess
+import psutil
 from config_module.config_io import (
     get_agent_executable_path,
     get_config_file_path,
-    get_service_executable_path
+    get_service_executable_path,
 )
 
 os_type = platform.system().lower()
@@ -18,7 +20,16 @@ if os_type == "windows":
     from rewst_windows_service import RewstWindowsService
 
 
-def get_service_name(org_id):
+def get_service_name(org_id: str) -> str:
+    """
+    Get the current service name of the using the latest platform.
+
+    Args:
+        org_id (str): Organization identifier in Rewst platform.
+
+    Returns:
+        str: Service name of the agent.
+    """
     if os_type == "windows":
         # Fetch the service name from the RewstWindowsService class
         return RewstWindowsService.get_service_name()
@@ -27,7 +38,16 @@ def get_service_name(org_id):
         return f"RewstRemoteAgent_{org_id}"
 
 
-def is_service_installed(org_id=None):
+def is_service_installed(org_id: str = None) -> bool:
+    """
+    Checks whether the agent service for an organization is installed or not.
+
+    Args:
+        org_id (str, optional): Organization identifier in Rewst platform. Defaults to None.
+
+    Returns:
+        bool: True if installed, otherwise False.
+    """
     service_name = get_service_name(org_id)
     if os_type == "windows":
         try:
@@ -42,21 +62,39 @@ def is_service_installed(org_id=None):
         logging.info(f"Service {service_name} is installed.")
         return os.path.exists(service_path)
     elif os_type == "darwin":
-        plist_path = f"{os.path.expanduser('~/Library/LaunchAgents')}/{service_name}.plist"
+        plist_path = (
+            f"{os.path.expanduser('~/Library/LaunchAgents')
+               }/{service_name}.plist"
+        )
         logging.info(f"Service {service_name} is installed.")
         return os.path.exists(plist_path)
 
 
-def is_service_running(org_id=None):
+def is_service_running(org_id: str = None) -> bool:
+    """
+    Checks whether the agent service for an organization is running or not.
+
+    Args:
+        org_id (str, optional): Organization identifier in Rewst platform. Defaults to None.
+
+    Returns:
+        bool: True if running, otherwise False.
+    """
     executable_path = get_agent_executable_path(org_id)
     executable_name = os.path.basename(executable_path)
-    for proc in psutil.process_iter(['pid', 'name']):
-        if proc.info['name'] == executable_name:
+    for proc in psutil.process_iter(["pid", "name"]):
+        if proc.info["name"] == executable_name:
             return executable_name
     return False
 
 
-def install_service(org_id):
+def install_service(org_id: str) -> None:
+    """
+    Installs the agent service for an organization.
+
+    Args:
+        org_id (str): Organization identifier in Rewst platform.
+    """
     executable_path = get_agent_executable_path(org_id)
     service_name = get_service_name(org_id)
 
@@ -69,17 +107,17 @@ def install_service(org_id):
             logging.info(f"Installing Windows Service: {service_name}")
             # install using service executable
             windows_service_path = get_service_executable_path(org_id)
-            subprocess.run(([windows_service_path,"install"]))
+            subprocess.run(([windows_service_path, "install"]))
 
         elif os_type == "linux":
             systemd_service_content = f"""
             [Unit]
             Description={service_name}
-    
+
             [Service]
             ExecStart={executable_path} --config-file {config_file_path}
             Restart=always
-    
+
             [Install]
             WantedBy=multi-user.target
             """
@@ -107,12 +145,23 @@ def install_service(org_id):
             </dict>
             </plist>
             """
-            with open(f"{os.path.expanduser('~/Library/LaunchAgents')}/{service_name}.plist", "w") as f:
+            with open(
+                f"{os.path.expanduser('~/Library/LaunchAgents')
+                   }/{service_name}.plist",
+                "w",
+            ) as f:
                 f.write(launchd_plist_content)
             subprocess.run(f"launchctl load {service_name}")
 
 
-def uninstall_service(org_id):
+def uninstall_service(org_id: str) -> None:
+    """
+    Uninstalls an agent service for an organization.
+
+    Args:
+        org_id (str): Organization identifier in Rewst platform.
+    """
+
     service_name = get_service_name(org_id)
     logging.info(f"Uninstalling service {service_name}.")
 
@@ -133,50 +182,74 @@ def uninstall_service(org_id):
         subprocess.run("systemctl daemon-reload")
 
     elif os_type == "darwin":
-        plist_path = f"{os.path.expanduser('~/Library/LaunchAgents')}/{service_name}.plist"
+        plist_path = (
+            f"{os.path.expanduser('~/Library/LaunchAgents')
+               }/{service_name}.plist"
+        )
         subprocess.run(f"launchctl unload {plist_path}")
         subprocess.run(f"rm {plist_path}")
 
 
-def check_service_status(org_id):
+def check_service_status(org_id: str) -> None:
+    """
+    Prints the status of an agent service for an organization to stdout.
+
+    Args:
+        org_id (str): Organization identifier in Rewst platform.
+    """
     service_name = get_service_name(org_id)
 
     if os_type == "windows":
         try:
             status = win32serviceutil.QueryServiceStatus(service_name)
-            print(f'Service status: {status[1]}')  # Printing the status to stdout
+            # Printing the status to stdout
+            print(f"Service status: {status[1]}")
         except Exception as e:
-            print(f'Error: {e}')
+            print(f"Error: {e}")
 
     elif os_type == "linux":
         try:
-            result = subprocess.run(['systemctl', 'is-active', service_name],
-                                    text=True, capture_output=True, check=True)
-            print(f'Service status: {result.stdout.strip()}')
+            result = subprocess.run(
+                ["systemctl", "is-active", service_name],
+                text=True,
+                capture_output=True,
+                check=True,
+            )
+            print(f"Service status: {result.stdout.strip()}")
         except subprocess.CalledProcessError as e:
-            print(f'Error: {e.stdout.strip()}')
+            print(f"Error: {e.stdout.strip()}")
 
         except Exception as e:
-            print(f'Error: {e}')
+            print(f"Error: {e}")
 
     elif os_type == "darwin":
         try:
-            result = subprocess.run(['launchctl', 'list', service_name],
-                                    text=True, capture_output=True, check=True)
+            result = subprocess.run(
+                ["launchctl", "list", service_name],
+                text=True,
+                capture_output=True,
+                check=True,
+            )
             if f"{service_name}" in result.stdout:
-                print(f'Service status: Running')
+                print(f"Service status: Running")
             else:
-                print(f'Service status: Not Running')
+                print(f"Service status: Not Running")
         except subprocess.CalledProcessError as e:
-            print(f'Error: {e.stdout.strip()}')
+            print(f"Error: {e.stdout.strip()}")
         except Exception as e:
-            print(f'Error: {e}')
+            print(f"Error: {e}")
 
     else:
-        print(f'Unsupported OS type: {os_type}')
+        print(f"Unsupported OS type: {os_type}")
 
 
-def start_service(org_id):
+def start_service(org_id: str) -> None:
+    """
+    Start the agent service for an organization.
+
+    Args:
+        org_id (str): Organization identifier in Rewst platform.
+    """
     service_name = get_service_name(org_id)
     logging.info(f"Starting Service {service_name} for {os_type}")
     if os_type == "windows":
@@ -187,7 +260,13 @@ def start_service(org_id):
         subprocess.run(f"launchctl start {service_name}")
 
 
-def stop_service(org_id):
+def stop_service(org_id: str) -> None:
+    """
+    Stop the agent service for an organization.
+
+    Args:
+        org_id (str): Organization identifier in Rewst platform.
+    """
     service_name = get_service_name(org_id)
     if os_type == "windows":
         if win32serviceutil.QueryServiceStatus(service_name):
@@ -201,6 +280,12 @@ def stop_service(org_id):
         subprocess.run(f"launchctl stop {service_name}")
 
 
-def restart_service(org_id):
+def restart_service(org_id: str) -> None:
+    """
+    Restart the agent service for an organization.
+
+    Args:
+        org_id (str): Organization identifier in Rewst platform.
+    """
     stop_service(org_id)
     start_service(org_id)
