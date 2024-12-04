d9eb9bf822d011ac9b8144201fb1e946dd588388
mlataza <1750947+mlataza@users.noreply.github.com>
2024-12-04 13:11:08 +0800

    fix coverage for service_management

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,16 +1,1 @@
 """ Global helper functions for testing """
-
-from pytest_mock import MockerFixture
-
-
-def mock_platform(mocker: MockerFixture, platform: str) -> None:
-    """
-    Mock the os_type variable for all scripts.
-
-    Args:
-        mocker (MockerFixture): Mocker fixture instance.
-        platform (str): Platform name in lowercase (e.g. windows, linux, darwin).
-    """
-    mocker.patch("config_module.config_io.os_type", platform)
-    mocker.patch("iot_hub_module.connection_management.os_type", platform)
-    mocker.patch("service_module.service_management.os_type", platform)
