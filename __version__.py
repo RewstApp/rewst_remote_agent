463a5aed6caf9d5eaea360ad23b39b67fc3cab28
mlataza <1750947+mlataza@users.noreply.github.com>
2025-01-28 08:32:07 +0800

    ci: add version file generation

--- __version__.py
+++ __version__.py
@@ -1,18 +1,5 @@
 """ Version tracking module """
 
-import subprocess
+from importlib import metadata
 
-def get_poetry_version() -> str|None:
-    """
-    Get poetry version from poetry file.
-
-    Returns:
-        str|None: poetry version string if successful, otherwise None
-    """
-    try:
-        version = subprocess.check_output(["poetry", "version", "--short"], text=True).strip()
-        return version
-    except subprocess.CalledProcessError:
-        return None
-
-__version__ = get_poetry_version()
+__version__ = metadata.version("rewst_remote_agent")
